bf0655493124fb733303fa65e4a34aa08cee56c3
cheroliv <cheroliv@users.noreply.github.com>
2026-06-13 12:12:48 +0200

    fix(iso): add --apt-secure false to lb config to bypass GPG transient errors

--- scripts/build-inner.sh
+++ scripts/build-inner.sh
@@ -55,6 +55,7 @@ if [[ ! -f "${BUILD_DIR}/config/common" ]]; then
         --mirror-bootstrap http://archive.ubuntu.com/ubuntu \
         --mirror-binary http://archive.ubuntu.com/ubuntu \
         --archive-areas 'main restricted universe multiverse' \
+        --apt-secure false \
         --bootappend-live 'boot=casper live-media=/dev/sr0 cow_backend=tmpfs username=magic hostname=magic-stick locales=fr_FR.UTF-8 keyboard-layouts=fr quiet splash console=ttyS0,115200' \
         --iso-volume "Magic Stick ${MAGIC_STICK_VERSION}" \
         --iso-publisher 'Magic Stick' \
